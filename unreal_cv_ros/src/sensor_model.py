Start to fix wrong scaling. Still need to determine right factor.

unreal_cv_ros/src/sensor_model.py
@@ -18,6 +18,11 @@ from struct import pack, unpack
 import time
 
 
+# DEBUGGING
+import pdb
+import matplotlib.pyplot as plt
+
+
 class SensorModel:
 
     def __init__(self):
@@ -75,7 +80,12 @@ class SensorModel:
         # Read out images
         img_color = np.asarray(PIL.Image.open(io.BytesIO(ros_data.color_data)))
         # img_depth = np.asarray(PIL.Image.open(io.BytesIO(ros_data.depth_data)))
-        img_depth = np.array(ros_data.depth_data).reshape((480,640))*64
+        # img_depth = np.array(ros_data.depth_data).reshape((480,640))*64 -> scaling issues
+        img_depth = np.array(ros_data.depth_data).reshape((480,640)) / 1000.0 # TODO: Needs to be checked/adapted correctly
+        # DEBUGGING
+        # plt.imshow(img_depth)
+        # plt.show()
+        # pdb.set_trace()
         # img_depth = np.asarray(ros_data.depth_data, dtype = np.float32).reshape((480, 640))
         #print(img_depth[0])
         # img_depth = np.load(io.BytesIO(ros_data.depth_data))

unreal_cv_ros/src/unreal_ros_client.py
@@ -2,7 +2,7 @@
 
 # Unrealcv
 from unrealcv import Client
-client = Client(('172.23.128.1',9000))
+client = Client(('172.20.0.1',9000))
 
 
 # ros
@@ -19,6 +19,10 @@ import sys
 import math
 import numpy as np
 import time
+
+# DEBUGGING
+import pdb
+import matplotlib.pyplot as plt
 
 
 class UnrealRosClient:
@@ -226,13 +230,20 @@ class UnrealRosClient:
         # Retrieve images
         # res_color = client.request('vget /camera/%d/lit png' % self.camera_id)
         # res_depth = client.request('vget /camera/%d/depth npy' % self.camera_id)
+        # res_color_orig = client.request('vget /camera/%d/lit png' % self.camera_id)
+        # res_depth_orig = client.request('vget /camera/%d/depth npy' % self.camera_id)
         # quick fix for New Maze environment, camera 1 is a fusion camera
         res_color = client.request('vget /camera/1/lit png')
         res_depth = client.request('vget /camera/1/depth npy')
+        pdb.set_trace()
         #res_depth = np.fromstring(res_depth[80:], dtype=np.float32, count=-1, sep='')
         res_depth = np.fromstring(res_depth[80:], dtype=np.float32, count=-1, sep='')
         # res_depth 
         # Publish data
+        # plt.imshow(res_depth.reshape(480,640))
+        # plt.show()
+        # pdb.set_trace()
+
         
         msg = UeSensorRaw()
         msg.header.stamp = header_stamp